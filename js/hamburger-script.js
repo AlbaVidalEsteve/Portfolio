75334f5a1243c33f3a950daed805fed511366148
AlbaVidalEsteve <66486832+AlbaVidalEsteve@users.noreply.github.com>
2024-02-12 17:54:00 +0100

    update js to repair dark mode

--- js/hamburger-script.js
+++ js/hamburger-script.js
@@ -1,18 +1,5 @@
-const BOTON= document.querySelector('#icono i');
-const ENLACES = document.querySelector('#enlaces');
 const SUN= document.querySelector('#sun');
 const MODE = document.querySelector('#mode');
-
-// window.addEventListener('load', ()=>{
-//     ENLACES.classList.remove('open');
-// });
-
-BOTON.addEventListener('click', ()=>{
-    ENLACES.classList.toggle('open');
-    setTimeout(() =>{
-        BOTON.classList.toggle('fa-xmark');
-    }, 600);
-});
 
 SUN.addEventListener('click', ()=>{
     MODE.classList.toggle('dark');
